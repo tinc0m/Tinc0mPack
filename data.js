8a2f2ec661ba2c00daf2c29fed29a4c08951f616
tinc0m <tincm371@gmail.com>
2026-04-05 00:36:57 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+// data.js - База текстур-паков Mpack
+
+const texturePacks = [
+    {
+        id: 1,
+        title: "WD Gaster RP",
+        shortDesc: "Зеленый мистический ресурс-пак",
+        fullDesc: "Ресурс-пак в стиле WD Gaster из Undertale. Меняет текстуры мобов на более зеленые и мистические. Добавляет звуковые эффекты для тотема.",
+        version: "1.19.4",
+        resolution: "x16",
+        preview: "textures/WD_Gaster_RP/preview.jpg",
+        zipFile: "textures/WD_Gaster_RP/WD_Gaster_RP.zip",
+        screenshots: []  // ← пока нет скринов, будет надпись "Soon"
+    }
+];
